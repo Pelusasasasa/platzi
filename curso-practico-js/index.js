7856bcf778744e43072d751e629dea4aa3079123
Pelusa-not <agustinlorenzatto@gmail.com>
2023-03-17 17:50:10 -0300

    Haciendo el curso practico de js

---
+++ curso-practico-js/index.js
@@ -0,0 +1,12 @@
+const h1 = document.querySelector('h1');
+const p = document.querySelector('p');
+const parrafoClass = document.getElementById('parrafo');
+const parrafoId = document.querySelector('.parrafo');
+const input = document.querySelector('input');
+
+console.log(h1);
+console.log(p);
+console.log(parrafoClass);
+console.log(parrafoId);
+console.log(input);
+
